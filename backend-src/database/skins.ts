a4fd8cf6d3265460038b435ebb2b7bef6622f5d7
Fedor Tarasenko <fedortarasenko2001@gmail.com>
2024-09-24 14:56:34 +0200

    Post request och Insertfuction på skins

--- backend-src/database/skins.ts
+++ backend-src/database/skins.ts
@@ -1,13 +1,23 @@
-import { Collection, WithId, Db } from 'mongodb'
+import { Collection, WithId, Db, ObjectId, InsertOneResult } from 'mongodb'
 import { Skin } from '../models/skin.js'
 import { connectToDatabase } from './db.js'
 
-async function getAllSkins(): Promise<WithId<Skin>[]> {
-  const db: Db = await connectToDatabase()
-  const col: Collection<Skin> = db.collection<Skin>('csskins')
+const db: Db = await connectToDatabase()
+const col: Collection<Skin> = db.collection<Skin>('csskins')
 
+async function getAllSkins(): Promise<WithId<Skin>[]> {
   const result: WithId<Skin>[] = await col.find({}).toArray()
   return result
 }
 
-export { getAllSkins }
+async function insertSkin(skin: Skin): Promise<ObjectId | null> {
+  try {
+    const result: InsertOneResult<Skin> = await col.insertOne(skin)
+    return result.insertedId
+  } catch (error) {
+    console.log('Error inserting skin: ', error)
+    return null
+  }
+}
+
+export { getAllSkins, insertSkin }
--- backend-src/routes/skins.ts
+++ backend-src/routes/skins.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Router } from 'express'
 
 import { Skin } from '../models/skin.js'
-import { getAllSkins } from '../database/skins.js'
+import { getAllSkins, insertSkin } from '../database/skins.js'
 import { WithId } from 'mongodb'
 
 export const router: Router = express.Router()
@@ -10,10 +10,20 @@ export const router: Router = express.Router()
 router.get('/', async (req: Request, res: Response<WithId<Skin>[]>) => {
   const allSkins: WithId<Skin>[] = await getAllSkins()
   res.send(allSkins)
-  res.sendStatus(200)
 })
 
 //POST
-// router.post('/', async (req: Request<Skin>, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const skin: Skin = req.body
+    const insertedId = await insertSkin(skin)
 
-// })
+    if (insertedId) {
+      res.status(201).send('Skin added succesfully')
+    } else {
+      res.status(400).send('Failed to add your skin')
+    }
+  } catch (error: any) {
+    res.status(500).send('An error occured' + error.message)
+  }
+})
